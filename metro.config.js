13a156373f980190431d5ef8f8956d39102f7c5f
sedama <sathwikareddy0799@gmail.com>
2024-03-01 22:42:24 -0600

    working app

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,15 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+// Disable Hermes for development
+defaultConfig.transformer = {
+  ...defaultConfig.transformer,
+  assetPlugins: ['react-native-reanimated/plugin'],
+};
+
 const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
